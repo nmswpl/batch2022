a96f355d19faec06c88b0ce33711f797a7eab680
admin <thilagavathis@nmsworks.co.in>
2022-10-07 18:16:46 +0530

    exercise1 programs

--- thilagavathi/src/main/java/in/co/nmsworks/exercise2/e13.java
+++ thilagavathi/src/main/java/in/co/nmsworks/exercise2/e13.java
@@ -1,2 +1,15 @@
-package in.co.nmsworks.exercise2;public class e13 {
-}
+//sum of the elements in the array
+
+package in.co.nmsworks.exercise2;
+
+public class e13 {
+    public static void main(String[] args) {
+        int arr[]=new int[]{4,2,4,5,6};
+        int sum=0,n;
+        for(int i=0; i<arr.length; i++){
+            sum=sum+arr[i];
+        }
+        System.out.println("sum of the numbers in the array:"+sum);
+        }
+    }
+
--- thilagavathi/src/main/java/in/co/nmsworks/exercise2/e3.java
+++ thilagavathi/src/main/java/in/co/nmsworks/exercise2/e3.java
@@ -1,2 +1,23 @@
-package in.co.nmsworks.exercise2;public class e3 {
+//product of the two arrays
+
+package in.co.nmsworks.exercise2;
+
+import java.util.Arrays;
+
+public class e3 {
+    public static void main(String[] args) {
+        int num1,num2;
+        String result="";
+        int arr1[]={2,4,1};
+        int arr2[]={3,5,7};
+        System.out.println("/n arr1"+Arrays.toString(arr1));
+        System.out.println("/n arr2"+Arrays.toString(arr2));
+        for(int i=0; i<arr1.length; i++){
+            num1=arr1[i];
+            num2= arr2[i];
+            result+= Integer.toString(num1*num2);
+        }
+        System.out.println("product of two arrays is:"+result);
+    }
 }
+
