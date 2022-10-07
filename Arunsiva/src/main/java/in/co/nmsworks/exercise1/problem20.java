9d6349a2fc73ca5a025b91b891dedc8362145171
admin <arunsivak@nmsworks.co.in>
2022-10-07 15:07:16 +0530

    Java Programs

---
+++ Arunsiva/src/main/java/in/co/nmsworks/exercise1/problem20.java
@@ -0,0 +1,11 @@
+package in.co.nmsworks.exercise1;
+
+//Calculate the power of a number using pow() function
+
+public class problem20 {
+    public static void main(String[] args) {
+        int base=2,power=5;
+        int result= (int) Math.pow(base,power);
+      System.out.println("Answer is = "+result);
+    }
+}
