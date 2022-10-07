a103a331039a11e9ab1331e096d61a6f095b83b3
SridharK11122000 <113993428+SridharK11122000@users.noreply.github.com>
2022-10-07 18:10:49 +0530

    Update Program20.java
    missing equal

--- nandagopal/src/main/java/in/co/nmsworks/excercise2/Program20.java
+++ nandagopal/src/main/java/in/co/nmsworks/excercise2/Program20.java
@@ -8,7 +8,7 @@ public class Program20 {
             System.out.println("Not Prime");
             return;
         }
-        for(int i=2; i<n/2; i++) {
+        for(int i=2; i<=n/2; i++) {
             if(n % i == 0) {
                 System.out.println("Not Prime");
                 return;
