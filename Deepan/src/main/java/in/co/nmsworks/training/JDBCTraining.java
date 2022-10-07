9ed1b5bcc09a4095356e3308dfff297ca6fd1da5
admin <deepanraje@nmsworks.co.in>
2022-10-07 12:25:52 +0530

    JDBC training exercise, created TodoList and added values to the database

---
+++ Deepan/src/main/java/in/co/nmsworks/training/JDBCTraining.java
@@ -0,0 +1,12 @@
+package in.co.nmsworks.training;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
+public class JdbcTraining {
+
+    public static void main(String[] args) {
+        Connection connect = DriverManager.getConnection("")
+    }
+}
