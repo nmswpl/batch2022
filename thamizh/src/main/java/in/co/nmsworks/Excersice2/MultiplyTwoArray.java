db1a69783e7d450b72b961417739a9da5d5bfbb8
admin <thamizharasans@nmsworks.co.in>
2022-10-07 17:26:02 +0530

    Thamizh Excersice2

---
+++ thamizh/src/main/java/in/co/nmsworks/Excersice2/MultiplyTwoArray.java
@@ -0,0 +1,17 @@
+package in.co.nmsworks.Excersice2;
+
+public class MultiplyTwoArray {
+    public static void main(String[] args) {
+
+
+
+    int [] a=new int[] {4,5,6};
+    int [] b=new int[] {2,5,10};
+
+    for(int i=0;i<a.length;i++)
+    {
+        System.out.print(a[i]*b[i]+" ");
+    }
+
+    }
+}
