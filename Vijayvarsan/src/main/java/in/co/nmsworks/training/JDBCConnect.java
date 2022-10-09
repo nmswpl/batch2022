7606974fee7a026603a028fe8a036c7b9d830990
admin <vijayvarsanm@nmsworks.co.in>
2022-10-09 17:57:14 +0530

    EXcersice 2 package

--- Vijayvarsan/src/main/java/in/co/nmsworks/training/JDBCConnect.java
+++ Vijayvarsan/src/main/java/in/co/nmsworks/training/JDBCConnect.java
@@ -1,2 +1,46 @@
-package in.co.nmsworks.training;public class JDBCConnect {
+package in.co.nmsworks.training;
+
+import java.sql.*;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class JDBCConnect {
+
+    public static void main(String[] args) {
+
+
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localost/Training");
+
+            PreparedStatement ps = con.prepareStatement("insert into Todo_List values(?,?,?,?);");
+
+            List<String[]> query = new ArrayList<>();
+            query.add(new String[]{"Vijayvarsan","Buying cloths","'2022-07-08 15:33:00'"});
+            query.add(new String[]{"VIART","Buying bat","'2022-07-08'"});
+            query.add(new String[]{"DOHNI","Buying GLOVES","'2022-07-08 04:33:00'"});
+            query.add(new String[]{"MESSI","WASHING DRESS","'2022-07-08 06:33:00'"});
+            query.add(new String[]{"RAINA","PRACTICING","'2022-07-08 01:33:00'"});
+            query.add(new String[]{"SURYA","INNOVATIVE","'2022-07-08 124:33:00'"});
+            query.add(new String[]{"BHUVI","USE THE WIND","'2022-07-08 11:33:00'"});
+            query.add(new String[]{"DRAVID","COACHING","'2022-07-08 09:33:00'"});
+            query.add(new String[]{"NEYMAR","SKILLS","'2022-07-08 19:33:00'"});
+            query.add(new String[]{"RONALDO","PLAY SOME OTHER GAMES","2022-07-08 02:22:00"});
+
+
+           for (int i= 0 ; i<10;i++) {
+               ps.setInt(1, i);
+               ps.setString(2, query.get(i)[0]);
+               ps.setString(3, query.get(i)[1]);
+               ps.setDate(4, Date.valueOf(query.get(i)[2]));
+
+               ps.executeUpdate();
+           }
+
+        } catch (SQLException e) {
+            System.err.println("NOT CONNECTED");
+            e.printStackTrace();
+        }
+    }
 }
--- Vijayvarsan/src/main/java/in/co/nmsworks/training/JDBCTraining.java
+++ Vijayvarsan/src/main/java/in/co/nmsworks/training/JDBCTraining.java
@@ -1,15 +1,46 @@
 package in.co.nmsworks.training;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.ArrayList;
 
-public class JdbcTraining {
+public class JDBCTraining {
 
     private void select()
     {
-        Connection con = DriverManager.getConnection("JDBC");
+
     }
     public static void main(String[] args) {
-        JdbcTraining jdbc = new JdbcTraining();
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/Training");
+//            String[] todoArray={"insert into Todo_List values(1,'VIRAT','PRACTICING BATTING','2022-10-05 04:30:22')",
+//                    "insert into Todo_List values(2,'MESSI','PRACTICE FOOT BALL','2022-10-05 06:30:22')",
+//                        "insert into Todo_List values(3,'BHUVI','PRACTICE BOWLING','2022-10-05 10:30:22')",
+//                    "insert into Todo_List values(4,'FEDD','TRAINING TENNIS','2022-10-05 12:30:22')",
+//                    "insert into Todo_List values(5,'VIJAY','SLEEPING','2022-10-05 21:30:22')"};
+            ArrayList<String> query= new ArrayList<>();
+            query.add("insert into Todo_List values(1,'VIRAT','PRACTICING BATTING','2022-10-05 04:30:22')");
+            query.add("insert into Todo_List values(2,'MESSI','PRACTICE FOOT BALL','2022-10-05 06:30:22')");
+            query.add("insert into Todo_List values(3,'BHUVI','PRACTICE BOWLING','2022-10-05 10:30:22')");
+            query.add( "insert into Todo_List values(4,'FEDD','TRAINING TENNIS','2022-10-05 12:30:22')");
+            query.add("insert into Todo_List values(5,'VIJAY','SLEEPING','2022-10-05 21:30:22')");
+
+
+
+            for (int i = 0; i < query.size(); i++) {
+                PreparedStatement ps = con.prepareStatement(query.get(i));
+                ps.executeUpdate();
+                System.out.println("connected");
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("NOT CONNECTED");
+            e.printStackTrace();
+        }
+
+        JDBCTraining jdbc = new JDBCTraining();
+
+
+
     }
 }
