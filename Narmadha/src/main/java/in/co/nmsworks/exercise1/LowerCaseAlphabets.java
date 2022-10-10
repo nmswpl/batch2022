082eee6b9a2df28d7004f2c00a867f2a7d6495f5
nmsadmin <narmadhas@nmsworks.co.in>
2022-10-10 11:52:01 +0530

    LowerCaseAlphabets

---
+++ Narmadha/src/main/java/in/co/nmsworks/exercise1/LowerCaseAlphabets.java
@@ -0,0 +1,10 @@
+package in.co.nmsworks.exercise1;
+
+public class LowerCaseAlphabets {
+    //Print lowercase alphabet using for loop
+    public static void main(String[] args) {
+        for (char c='a';c<='z';c++){
+            System.out.print(c+" ");
+        }
+    }
+}
