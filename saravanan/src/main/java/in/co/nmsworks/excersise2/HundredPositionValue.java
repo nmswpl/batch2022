1a9b1e357f472639fcbce99829ca970565edba18
saravanan <saravanans@nmsworks.co.in>
2022-10-10 10:55:00 +0530

    change made

--- saravanan/src/main/java/in/co/nmsworks/excersise2/HundredPositionValue.java
+++ saravanan/src/main/java/in/co/nmsworks/excersise2/HundredPositionValue.java
@@ -5,7 +5,7 @@ public class HundredPositionValue
     public static void main(String[] args)
     {
       int number =580;
-      //===========================
+      //============================
       int ans =number/100;
       String[] arr= {"zero","one","two","three","four","five","six","seven"};
       for(int i=0;i< arr.length;i++)
