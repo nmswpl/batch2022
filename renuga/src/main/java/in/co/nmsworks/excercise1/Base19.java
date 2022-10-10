d977982d68d624686d4709b7bec33e0cb4667204
renuga <renugag@nmsworks.co.in>
2022-10-10 10:54:04 +0530

    changes done to fix program
    commit the file into program

---
+++ renuga/src/main/java/in/co/nmsworks/excercise1/Base19.java
@@ -0,0 +1,15 @@
+package in.co.nmsworks.excercise1;
+
+public class Base19 {
+    public static void main(String[] args) {
+        int base = 3, exponent = 4;
+
+        long result = 1;
+
+        for (; exponent != 0; --exponent) {
+            result *= base;
+        }
+
+        System.out.println("Answer = " + result);
+    }
+}
