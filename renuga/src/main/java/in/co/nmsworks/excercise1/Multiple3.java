8bbe9e72d7a08915176d7ca284cfbde18a18d7eb
renuga <renugag@nmsworks.co.in>
2022-10-10 11:06:46 +0530

    changes done to fix program
    commit the file into program

--- renuga/src/main/java/in/co/nmsworks/excercise1/Multiple3.java
+++ renuga/src/main/java/in/co/nmsworks/excercise1/Multiple3.java
@@ -1,5 +1,5 @@
 package in.co.nmsworks.excercise1;
-
+/*Write a program to Multiply Two Floating-Point Numbers*/
 public class Multiple3 {
     public static void main(String[] args) {
         float a= 10.0f;
