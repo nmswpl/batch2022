3e75bd3dc981dd4ac47367bcb42382faffd5ab29
renuga <renugag@nmsworks.co.in>
2022-10-10 14:12:17 +0530

    changes done to fix program
    commit the file into program

--- renuga/src/main/java/in/co/nmsworks/excercise1/Positive10.java
+++ renuga/src/main/java/in/co/nmsworks/excercise1/Positive10.java
@@ -2,6 +2,9 @@ package in.co.nmsworks.excercise1;
 
 import java.util.Scanner;
 
+
+/* 10. Write a program to Check if a Number is Positive or Negative using if else */
+
 public class Positive10 {
     public static void main(String[] args) {
         int number;
