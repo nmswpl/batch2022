d27e5be576a8cfff4e6550c864662749a6f011d1
VijayvarsanM <vijayvarsanm@nmsworks.co.in>
2022-10-10 10:52:45 +0530

    46 programs commit with some alteration

--- Vijayvarsan/src/main/java/in/co/nmsworks/training/JDBCConnect.java
+++ Vijayvarsan/src/main/java/in/co/nmsworks/training/JDBCConnect.java
@@ -9,38 +9,48 @@ import java.util.List;
 public class JDBCConnect {
 
     public static void main(String[] args) {
-
-
+        Connection con=null;
+        PreparedStatement ps=null;
         try {
-            Connection con = DriverManager.getConnection("jdbc:mysql://localost/Training");
-
-            PreparedStatement ps = con.prepareStatement("insert into Todo_List values(?,?,?,?);");
-
+            con= DriverManager.getConnection("jdbc:mysql://localhost/Training");
             List<String[]> query = new ArrayList<>();
-            query.add(new String[]{"Vijayvarsan","Buying cloths","'2022-07-08 15:33:00'"});
-            query.add(new String[]{"VIART","Buying bat","'2022-07-08'"});
-            query.add(new String[]{"DOHNI","Buying GLOVES","'2022-07-08 04:33:00'"});
-            query.add(new String[]{"MESSI","WASHING DRESS","'2022-07-08 06:33:00'"});
-            query.add(new String[]{"RAINA","PRACTICING","'2022-07-08 01:33:00'"});
-            query.add(new String[]{"SURYA","INNOVATIVE","'2022-07-08 124:33:00'"});
-            query.add(new String[]{"BHUVI","USE THE WIND","'2022-07-08 11:33:00'"});
-            query.add(new String[]{"DRAVID","COACHING","'2022-07-08 09:33:00'"});
-            query.add(new String[]{"NEYMAR","SKILLS","'2022-07-08 19:33:00'"});
+            query.add(new String[]{"Vijayvarsan","Buying cloths","2022-07-08 15:33:00"});
+            query.add(new String[]{"VIART","Buying bat","2022-07-08 05:12:33"});
+            query.add(new String[]{"DOHNI","Buying GLOVES","2022-07-08 04:33:00"});
+            query.add(new String[]{"MESSI","WASHING DRESS","2022-07-08 06:33:00"});
+            query.add(new String[]{"RAINA","PRACTICING","2022-07-08 01:33:00"});
+            query.add(new String[]{"SURYA","INNOVATIVE","2022-07-08 24:33:00"});
+            query.add(new String[]{"BHUVI","USE THE WIND","2022-07-08 11:33:00"});
+            query.add(new String[]{"DRAVID","COACHING","2022-07-08 09:33:00"});
+            query.add(new String[]{"NEYMAR","SKILLS","2022-07-08 19:33:00"});
             query.add(new String[]{"RONALDO","PLAY SOME OTHER GAMES","2022-07-08 02:22:00"});
+           ps = con.prepareStatement("insert into Todo_List values(?,?,?,?)");
 
 
-           for (int i= 0 ; i<10;i++) {
-               ps.setInt(1, i);
+            System.out.println("qury size"+query.size());
+           for (int i= 0 ; i< query.size();i++) {
+               ps.setInt(1, i+1);
                ps.setString(2, query.get(i)[0]);
                ps.setString(3, query.get(i)[1]);
-               ps.setDate(4, Date.valueOf(query.get(i)[2]));
-
+               ps.setString(4, query.get(i)[2]);
                ps.executeUpdate();
            }
 
         } catch (SQLException e) {
             System.err.println("NOT CONNECTED");
             e.printStackTrace();
+        }
+        finally {
+            try{
+            if(ps!=null)
+                ps.close();
+            if(con!=null)
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.err.println("Exception");
+            }
         }
     }
 }
