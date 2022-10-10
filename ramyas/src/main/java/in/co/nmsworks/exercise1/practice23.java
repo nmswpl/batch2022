1801feee96a9ff62a5bc635483175ac7e16e2385
sramya <sramya@nmsworks.co.in>
2022-10-10 23:57:38 +0530

    Exercise 3 file reader and duplicates

--- ramyas/src/main/java/in/co/nmsworks/exercise1/practice23.java
+++ ramyas/src/main/java/in/co/nmsworks/exercise1/practice23.java
@@ -1,18 +1,19 @@
 package in.co.nmsworks.exercise1;
+
 //Write a program to find Factorial of a Number Using Recursion
 public class practice23
 {
-    public static void main(String[] args)
-    {
-      int no = 6;
-      int fact =  factorial(no);
+   public static void main(String[] args) {
+     int no = 6;
+     int fact =  factorial(no);
       System.out.println(fact);
-    }
-    public static int factorial(int no)
-    {
-        if(no >= 1)
-         return no * factorial(no - 1);
-        else
-            return 1;
-    }
-}
+   }
+  public static int factorial(int no)
+   {
+      if(no==1)
+          return 1;
+      else
+         return no*factorial(no-1);
+     }
+
+   }
