e3c322679a034df490fe57e2347a4f81fef7d250
Ajanthamahalakshmi <Ajanthamahalakshmi@nmsworks.co.in>
2022-10-10 14:14:29 +0530

    changes made

--- Ajanthamahalakshmi/src/main/java/in/co/nmsworks/training/excercise1/AlphabetVowelConstant.java
+++ Ajanthamahalakshmi/src/main/java/in/co/nmsworks/training/excercise1/AlphabetVowelConstant.java
@@ -4,9 +4,9 @@ public class AlphabetVowelConstant {
     public static void main(String[] args)
     {
 
-            int ch = 'a';
+            char ch = 'A';
 
-            if(ch == 'a'| ch == 'e'| ch == 'i'|ch == 'o'|ch == 'u')
+            if(ch == 'a'| ch == 'e'| ch == 'i'|ch == 'o'|ch == 'u'|ch == 'A'|ch == 'E'| ch == 'I' | ch == 'O'| ch == 'U')
             {
                 System.out.println("entered value is vowel");
             }
--- Ajanthamahalakshmi/src/main/java/in/co/nmsworks/training/excercise2/CharacterVowelConsonant.java
+++ Ajanthamahalakshmi/src/main/java/in/co/nmsworks/training/excercise2/CharacterVowelConsonant.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class CharacterVowelConsonant {
     public static void main(String[] args) {
-        int ch = 'b';
+        int ch = 'Z';
 
-        if(ch == 'a'| ch == 'e'| ch == 'i'|ch == 'o'|ch == 'u')
+        if(ch == 'a'| ch == 'e'| ch == 'i'|ch == 'o'|ch == 'u'|ch == 'A'|ch == 'E'| ch == 'I' | ch == 'O'| ch == 'U')
         {
             System.out.println("enter value is vowel");
         }
