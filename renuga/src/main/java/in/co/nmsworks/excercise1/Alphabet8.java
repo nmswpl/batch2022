51598a0ae93152124332b9629bc564c1566861bc
renuga <renugag@nmsworks.co.in>
2022-10-10 14:08:22 +0530

    changes done to fix program
    commit the file into program

--- renuga/src/main/java/in/co/nmsworks/excercise1/Alphabet8.java
+++ renuga/src/main/java/in/co/nmsworks/excercise1/Alphabet8.java
@@ -1,5 +1,8 @@
 package in.co.nmsworks.excercise1;
 
+/* 8.Write a program to Check whether an alphabet is vowel or consonant using if..else
+        statement*/
+
 public class Alphabet8 {
     public static void main(String[] args) {
      char ch ='a';
