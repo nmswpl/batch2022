8977e0cd39d44269f5c494326c65e5ec66e71298
pradeepkumar <pradeepkumar@nmsworks.co.in>
2022-10-10 10:47:45 +0530

    this is committed

---
+++ pradeepkumar/src/main/java/in/co/nmsworks/exercise2/EndBy7.java
@@ -0,0 +1,13 @@
+package in.co.nmsworks.exercise2;
+
+public class EndBy7 {
+    public static void main(String[] args) {
+        for (int i = 200; i <=500 ; i++) {
+            if (i%10==7)
+            {
+                System.out.print(i + " ");
+            }
+            
+        }
+    }
+}
