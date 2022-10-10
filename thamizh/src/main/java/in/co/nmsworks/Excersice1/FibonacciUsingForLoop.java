ec544ae701010b06938dd876ee5fc1bf41fe5195
ThamizharasanS <thamizharasans@nmsworks.co.in>
2022-10-10 10:47:57 +0530

    changes in fibo program

--- thamizh/src/main/java/in/co/nmsworks/Excersice1/FibonacciUsingForLoop.java
+++ thamizh/src/main/java/in/co/nmsworks/Excersice1/FibonacciUsingForLoop.java
@@ -18,10 +18,11 @@ public class FibonacciUsingForLoop {
         for(int i=0;i<n;i++)
         {
             n3=n1+n2;
+            System.out.print(n1+" ");
             n1=n2;
             n2=n3;
 
-            System.out.println(n3);
+
         }
     }
 }
