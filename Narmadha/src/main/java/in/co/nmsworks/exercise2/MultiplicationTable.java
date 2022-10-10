0109bc994edfbcebfbc2e6452ee71315298aae73
nmsadmin <narmadhas@nmsworks.co.in>
2022-10-10 14:07:34 +0530

    multiplication table of 17 upTo 10 numbers

---
+++ Narmadha/src/main/java/in/co/nmsworks/exercise2/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package in.co.nmsworks.exercise2;
+
+public class MultiplicationTable {
+    //program to print the multiplication table of 17 upTo 10 numbers.
+    public static void main(String[] args) {
+        int num=17;
+        for(int i=1;i<=10;i++){
+            System.out.println(num+"*"+i+"="+num*i);
+
+        }
+    }
+}
