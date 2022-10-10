4ade8b17a9b2ec2dbac4191c2044efbcbe579164
Ajanthamahalakshmi <Ajanthamahalakshmi@nmsworks.co.in>
2022-10-10 11:59:27 +0530

    changes made

--- Ajanthamahalakshmi/src/main/java/in/co/nmsworks/training/excercise2/ArrayReverse.java
+++ Ajanthamahalakshmi/src/main/java/in/co/nmsworks/training/excercise2/ArrayReverse.java
@@ -2,7 +2,7 @@ package in.co.nmsworks.training.excercise2;
 
 public class ArrayReverse {
     public static void main(String[] args) {
-        String[] arr = {"a","b","c","d"};
+        String[] arr = {"a","b","c","d","e"};
         int s = arr.length;
         System.out.println(arr.length);
         for(int i = s-1 ; i >= 0 ; i--)
