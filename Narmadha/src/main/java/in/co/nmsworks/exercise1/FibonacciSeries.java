76a319cfa8b358efcd56175923f2fa4935bddb0a
nmsadmin <narmadhas@nmsworks.co.in>
2022-10-10 11:50:17 +0530

    FibonacciSeries program

---
+++ Narmadha/src/main/java/in/co/nmsworks/exercise1/FibonacciSeries.java
@@ -0,0 +1,20 @@
+package in.co.nmsworks.exercise1;
+
+public class FibonacciSeries {
+    //Display Fibonacci Series Using for Loop
+    public static void main(String[] args) {
+        int n1=0;
+        int n2=1;
+        int count=10;
+        System.out.print(n1+ " "+n2+" ");
+
+        for(int i=2;i<count;i++){
+           int n3=n1+n2;
+            n1=n2;
+            n2=n3;
+            System.out.print( n3+" ");
+
+        }
+
+    }
+}
