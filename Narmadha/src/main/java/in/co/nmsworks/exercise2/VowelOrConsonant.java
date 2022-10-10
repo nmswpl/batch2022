8dcc8b570c754c1aa4fc67b7a279a4b3fa9ca4a8
nmsadmin <narmadhas@nmsworks.co.in>
2022-10-10 11:01:02 +0530

    vowel or consonant program

---
+++ Narmadha/src/main/java/in/co/nmsworks/exercise2/VowelOrConsonant.java
@@ -0,0 +1,20 @@
+package in.co.nmsworks.exercise2;
+
+
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+    //program to check if a given Character is a Vowel or Consonant.
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a character : ");
+        char ch=sc.next().charAt(0);
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'&& ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+            System.out.println(ch + " is a vowel");
+        }
+        else{
+            System.out.println(ch + " is a consonant");
+        }
+
+    }
+}
