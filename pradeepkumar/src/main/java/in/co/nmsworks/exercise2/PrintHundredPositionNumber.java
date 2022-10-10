71707bf008f0620b1581ee4758e5c004cf363cdf
pradeepkumar <pradeepkumar@nmsworks.co.in>
2022-10-10 14:04:54 +0530

    FInd occurrence in an array If block will be added. Hundred position prgm program will be newly added

--- pradeepkumar/src/main/java/in/co/nmsworks/exercise2/PrintHundredPositionNumber.java
+++ pradeepkumar/src/main/java/in/co/nmsworks/exercise2/PrintHundredPositionNumber.java
@@ -3,6 +3,16 @@ package in.co.nmsworks.exercise2;
 public class PrintHundredPositionNumber {
     public static void main(String[] args) {
         int value=555;
+        int div=value/100;
+        System.out.println(div);
+        String[] arr={"zero","one","two","three","four","five","six","seven","eight","nine"};
+        for (int i = 0; i < arr.length; i++) {
+            if (div==i)
+            {
+                System.out.println(arr[i]);
+            }
+
+        }
 
 
     }
