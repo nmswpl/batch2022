551b996c5bc27d948a8168f596abedba1d40b693
VigneshV <vigneshv@nmsworks.co.in>
2022-10-10 16:39:10 +0530

    Find Duplicates in a File

--- vigneshV/src/main/java/in/co/nmsworks/exercisethree/FileHandler.java
+++ vigneshV/src/main/java/in/co/nmsworks/exercisethree/FileHandler.java
@@ -1,27 +1,39 @@
 package in.co.nmsworks.exercisethree;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class FileHandler
 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+    {
         ReadFile filereader = new ReadFile();
         List<Integer> numbers = new ArrayList<>(filereader.fileParser("/home/nmsadmin/Downloads/Numbers.txt"));
         searchDuplicate(numbers);
+        //findOccurance(numbers);
 
 
     }
 
+//    private static void findOccurance(List<Integer> numbers)
+//    {
+//        Map<Integer,Integer> map = new HashMap<>();
+//        for(int i :numbers)
+//            map.put(i,map.getOrDefault(i,0)+1);
+//        System.out.println(map);
+//    }
+
     private static void searchDuplicate(List<Integer> numbers)
     {
+
         ArrayList<Integer> duplicateNum = new ArrayList<>();
         for(int i:numbers)
         {
             if(!duplicateNum.contains(i))
                 duplicateNum.add(i);
+            else
+                System.out.print(" "+i);
         }
-        System.out.println("DuplicateNumbers "+duplicateNum);
+
     }
 }
