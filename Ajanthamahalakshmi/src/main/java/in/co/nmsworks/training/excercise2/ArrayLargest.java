97b684763f91d25754c498d5ce2354b1b52fa13a
Ajanthamahalakshmi <Ajanthamahalakshmi@nmsworks.co.in>
2022-10-10 11:59:00 +0530

    changes made

--- Ajanthamahalakshmi/src/main/java/in/co/nmsworks/training/excercise2/ArrayLargest.java
+++ Ajanthamahalakshmi/src/main/java/in/co/nmsworks/training/excercise2/ArrayLargest.java
@@ -2,7 +2,7 @@ package in.co.nmsworks.training.excercise2;
 
 public class ArrayLargest {
     public static void main(String[] args) {
-        int[] arr = { 10,23,3, 78,95};
+        int[] arr = { 100,23,3, 78,95};
         int maxi = arr[0];
         for( int i =0; i < arr.length; i++)
         {
