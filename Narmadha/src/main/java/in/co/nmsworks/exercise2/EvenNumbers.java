0fe5d0c8cbd92a7369cf2a0496d248cb870b56d6
nmsadmin <narmadhas@nmsworks.co.in>
2022-10-10 13:58:18 +0530

    all even numbers  in descending order

---
+++ Narmadha/src/main/java/in/co/nmsworks/exercise2/EvenNumbers.java
@@ -0,0 +1,14 @@
+package in.co.nmsworks.exercise2;
+
+public class EvenNumbers {
+   // program to print all even numbers between 601 and 700 in descending order
+    public static void main(String[] args) {
+        for(int i=700;i>601;i--){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+
+
+    }
+}
