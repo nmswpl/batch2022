d46f8b5bd5909cce90330223934d5978605f3da4
Arunsiva K <arunsivak@nmsworks.co.in>
2022-10-11 17:26:29 +0530

    Exercise3 NoOfRepeat also added

--- Arunsiva/src/main/java/in/co/nmsworks/exercise3/NoOfOcc.java
+++ Arunsiva/src/main/java/in/co/nmsworks/exercise3/NoOfOcc.java
@@ -1,53 +1,59 @@
 package in.co.nmsworks.exercise3;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class NoOfOcc {
 
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         List<Integer> val = BufferReader.Readfile("/home/nmsadmin/Downloads/Numbers.txt");
 
-        Map<Integer,Integer> mapcountlist=new HashMap<>();
+        Map<Integer, Integer> mapcountlist = new HashMap<>();
 
-        for(int x:val){
 
-            if(mapcountlist.containsKey(x))
-            {
-                mapcountlist.put(x,(mapcountlist.get(x)+1));
+        for (int x : val) {
 
-            }
-            else
-            {
-                  mapcountlist.put(x,1);
+            if (mapcountlist.containsKey(x)) {
+                mapcountlist.put(x, (mapcountlist.get(x) + 1));
+
+            } else {
+                mapcountlist.put(x, 1);
             }
         }
 
         System.out.println(mapcountlist);
         System.out.println(mapcountlist.size());
 
+        //-------------------------------No Of Times------------------------------------------
 
+        Map<Integer, List<Integer>> mapnooftimes = new HashMap<>();
 
+        for (int valuemap : mapcountlist.keySet()) {
+            int num = mapcountlist.get(valuemap);
+            if(mapnooftimes.containsKey(num))
+            {
+                mapnooftimes.get(num).add(valuemap);
+            }
 
-//
-//        for(int x:val)
-//        {
-//            int count = 0;
-//            for(int y:val)
-//            {
-//                if(x==y)
-//                {
-//                    count++;
-//                }
-//            }
-//            mapcountlist.put(x,count);
-//        }
-//        System.out.println(mapcountlist);
-//        System.out.println(mapcountlist.size());
+          else
+            {
+                List<Integer> finallist = new ArrayList<>();
+                finallist.add(num);
+                mapnooftimes.put(num,finallist);
+            }
+        }
+        System.out.println(mapnooftimes);
     }
-    
 }
+
+
+
+
+
+
+    
+
 
