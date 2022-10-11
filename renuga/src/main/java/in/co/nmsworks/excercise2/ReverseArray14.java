91ee022d924a630613fafe1f2e7ac092accb91ed
renuga <renugag@nmsworks.co.in>
2022-10-11 07:46:42 +0530

    changes done to fix program

--- renuga/src/main/java/in/co/nmsworks/excercise2/ReverseArray14.java
+++ renuga/src/main/java/in/co/nmsworks/excercise2/ReverseArray14.java
@@ -8,7 +8,7 @@ public class ReverseArray14 {
         System.out.println("Enter a string array ");
         String text = scanner.nextLine();
         char[] arr = text.toCharArray();
-//your code goes here
+
         int n=arr.length;
         String str="";
         for(int i=n-1 ; i>-1 ; i--){
