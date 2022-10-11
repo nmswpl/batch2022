f308e933150c1d708ae34de85946075aacc83c02
renuga <renugag@nmsworks.co.in>
2022-10-11 07:35:24 +0530

    changes done to fix program
    commit the file into program

---
+++ renuga/src/main/java/in/co/nmsworks/excercise3/FileParsing.java
@@ -0,0 +1,27 @@
+package in.co.nmsworks.excercise3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileParsing {
+    public static List<Integer> readFileAndGetIntList(String path) {
+        List<Integer> arrayList = new ArrayList<>();
+
+        try {
+            FileReader fileReader = new FileReader(path);
+            BufferedReader bf = new BufferedReader(fileReader);
+
+            String line;
+            while ((line = bf.readLine()) != null) {
+
+                arrayList.add(Integer.parseInt(line));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return arrayList;
+    }
+}
