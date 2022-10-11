387605ddcde6844ea1478697f64cb580224eccba
saravanan <saravanans@nmsworks.co.in>
2022-10-11 15:06:50 +0530

    UserDetails get from mysql and write into new text file

---
+++ saravanan/src/main/java/in/co/nmsworks/excersise4/ReadDataFromMysqlWriteIntoNewFile.java
@@ -0,0 +1,38 @@
+package in.co.nmsworks.excersise4;
+
+import java.io.FileWriter;
+import java.sql.*;
+
+public class ReadDataFromMysqlWriteIntoNewFile
+{
+    public static void main(String[] args)
+    {
+         try(FileWriter writeIntoMaleDetails = new FileWriter("/home/nmsadmin/Downloads/userdetailsofmaleonly.txt");
+             FileWriter writeIntoFemaleDetails = new FileWriter("/home/nmsadmin/Downloads/userdetailsfemaleonly.txt");
+             Connection con = DriverManager.getConnection("jdbc:mysql://localhost/saro"))
+         {
+             PreparedStatement sts = con.prepareStatement("select * from user_details");
+
+             ResultSet resultTable = sts.executeQuery();
+            // ResultSetMetaData columnName = resultTable.getMetaData();
+             while (resultTable.next())
+             {
+                // int sqlColumn1 = resultTable.getInt(1);
+                 String sqlColumn2 = resultTable.getString(2);
+                 String sqlColumn3 = resultTable.getString(3);
+                 String sqlColumn4 = resultTable.getString(4);
+                 String sqlColumn5 = resultTable.getString(5);
+                 if(sqlColumn5.equalsIgnoreCase("male"))
+                      writeIntoMaleDetails.write(sqlColumn2+"  "+sqlColumn3 +" "+sqlColumn4+" "+sqlColumn5+"\n");
+                 else
+                     writeIntoFemaleDetails.write(sqlColumn2+"  "+sqlColumn3 +" "+sqlColumn4+" "+sqlColumn5+"\n");
+
+             }
+         }
+         catch (Exception e)
+         {
+             System.out.println("does not read a data and store into new file");
+         }
+    }
+
+}
