1c6852bf58c9e515e3fd3417a34357a8d3c00980
saravanan <saravanans@nmsworks.co.in>
2022-10-11 17:26:18 +0530

    read data from mysql and write into new file

--- saravanan/src/main/java/in/co/nmsworks/excersise4/ReadDataFromMysqlWriteIntoNewFile.java
+++ saravanan/src/main/java/in/co/nmsworks/excersise4/ReadDataFromMysqlWriteIntoNewFile.java
@@ -1,5 +1,6 @@
 package in.co.nmsworks.excersise4;
 
+
 import java.io.FileWriter;
 import java.sql.*;
 
@@ -7,12 +8,11 @@ public class ReadDataFromMysqlWriteIntoNewFile
 {
     public static void main(String[] args)
     {
-         try(FileWriter writeIntoMaleDetails = new FileWriter("/home/nmsadmin/Downloads/userdetailsofmaleonly.txt");
-             FileWriter writeIntoFemaleDetails = new FileWriter("/home/nmsadmin/Downloads/userdetailsfemaleonly.txt");
+         try(FileWriter writeMaleDetails = new FileWriter("/home/nmsadmin/Downloads/userdetailsofmaleonly.txt");
+             FileWriter writeFemaleDetails = new FileWriter("/home/nmsadmin/Downloads/userdetailsfemaleonly.txt");
              Connection con = DriverManager.getConnection("jdbc:mysql://localhost/saro");
             PreparedStatement sts = con.prepareStatement("select * from user_details"))
          {
-
              ResultSet resultTable = sts.executeQuery();
             // ResultSetMetaData columnName = resultTable.getMetaData();
              while (resultTable.next())
@@ -23,9 +23,9 @@ public class ReadDataFromMysqlWriteIntoNewFile
                  String sqlColumn4 = resultTable.getString(4);
                  String sqlColumn5 = resultTable.getString(5);
                  if(sqlColumn5.equalsIgnoreCase("male"))
-                      writeIntoMaleDetails.write(sqlColumn2+"  "+sqlColumn3 +" "+sqlColumn4+" "+sqlColumn5+"\n");
+                      writeMaleDetails.write(sqlColumn2+"  "+sqlColumn3 +" "+sqlColumn4+" "+sqlColumn5+"\n");
                  else
-                     writeIntoFemaleDetails.write(sqlColumn2+"  "+sqlColumn3 +" "+sqlColumn4+" "+sqlColumn5+"\n");
+                     writeFemaleDetails.write(sqlColumn2+"  "+sqlColumn3 +" "+sqlColumn4+" "+sqlColumn5+"\n");
              }
          }
          catch (Exception e)
