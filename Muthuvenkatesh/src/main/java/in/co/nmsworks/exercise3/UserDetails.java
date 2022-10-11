c6bb05b7005b9b65f65d8a24156fdef2526dd894
Muthu Venkatesh M <muthuvenkateshm@nmsworks.co.in>
2022-10-11 22:46:47 +0530

    seperate male and female users in user_details table

---
+++ Muthuvenkatesh/src/main/java/in/co/nmsworks/exercise3/UserDetails.java
@@ -0,0 +1,67 @@
+package in.co.nmsworks.exercise3;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDetails {
+    public static void main(String[] args) {
+        maleUser();
+        femaleUser();
+    }
+    public static void maleUser(){
+        File file = new File("/home/nmsadmin/Downloads/male user.csv");
+        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost/Training");
+             PreparedStatement st = con.prepareStatement("SELECT * FROM user_details WHERE gender = 'Male';");
+             FileWriter writer = new FileWriter(file)) {
+
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("user_id");
+                String name = rs.getString("username");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+                String gender = rs.getString("gender");
+                String password = rs.getString("password");
+                String status = rs.getString("status");
+
+                String row = id + " " + name + " " + firstName + " " + lastName + " " + gender + " " + password + " " + status;
+                writer.write(row + "\n");
+
+            }
+        } catch (Exception e) {
+            System.err.println("connection lost");
+            e.printStackTrace();
+        }
+
+    }
+    public static void femaleUser(){
+        File file = new File("/home/nmsadmin/Downloads/female user.csv");
+        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost/Training");
+             PreparedStatement st = con.prepareStatement("SELECT * FROM user_details WHERE gender = 'Female';");
+             FileWriter writer = new FileWriter(file)) {
+
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("user_id");
+                String name = rs.getString("username");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+                String gender = rs.getString("gender");
+                String password = rs.getString("password");
+                String status = rs.getString("status");
+
+                String row = id + " " + name + " " + firstName + " " + lastName + " " + gender + " " + password + " " + status;
+                writer.write(row + "\n");
+
+            }
+        } catch (Exception e) {
+            System.err.println("connection lost");
+            e.printStackTrace();
+        }
+
+    }
+}
