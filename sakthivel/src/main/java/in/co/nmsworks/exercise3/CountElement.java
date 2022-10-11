c394cc6dbc588b993cafc99954154b60ff0ef9d7
sakthivel <sakthivelm@nmsworks.co.in>
2022-10-11 17:11:02 +0530

    File Reader and Duplicate Element Count, Number Count

--- sakthivel/src/main/java/in/co/nmsworks/exercise3/CountElement.java
+++ sakthivel/src/main/java/in/co/nmsworks/exercise3/CountElement.java
@@ -1,5 +1,6 @@
 package in.co.nmsworks.exercise3;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,34 +9,58 @@ import static in.co.nmsworks.exercise3.DuplicateNumber.duplicateNumber;
 
 public class CountElement
 {
-    public static Map<Integer,Integer> CountInteger( List<Integer> list) throws Exception
+    public static Map<Integer,Integer> countInteger( List<Integer> list) throws Exception
     {
+        int value;
         Map<Integer, Integer> numberCountMap = new HashMap<>();
+
+        Map<Integer,List<Integer>> elementlist=new HashMap<>();
+
         for (int i = 0; i < list.size(); i++)
         {
 
             if (numberCountMap.containsKey(list.get(i)))
             {
-                int value=numberCountMap.get(list.get(i));
+                 value=numberCountMap.get(list.get(i));
                 value++;
-                numberCountMap.put(list.get(i), value);
+                //numberCountMap.put(list.get(i), value);
             }
-            else
-            {
+            else {
                 numberCountMap.put(list.get(i), 1);
-
             }
         }
         System.out.println(numberCountMap);
+
+
+
+        for(Integer a : numberCountMap.keySet())
+        {
+            Integer temp = numberCountMap.get(a);
+            if(elementlist.containsKey(temp))
+            {
+                elementlist.get(temp).add(a);
+
+            }
+            else
+            {
+                List<Integer> element=new ArrayList<>();
+                element.add(a);
+                elementlist.put(temp,element);
+            }
+        }
+        System.out.println(elementlist);
+
+
         return numberCountMap;
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
 
         FileRead file = new FileRead();
         List<Integer> list = file.fileRead("/home/nmsadmin/Downloads/Numbers.txt");
         duplicateNumber(list);
-        CountInteger(list);
+        countInteger(list);
     }
 
 }
