482b362a3b9d2c76a8ca50587aaae23abc354e20
kabila <kabilam@nmsworks.co.in>
2022-10-11 09:10:02 +0530

    update

--- kabila/src/main/java/in/co/nmsworks/training/Jdbc.java
+++ kabila/src/main/java/in/co/nmsworks/training/Jdbc.java
@@ -1,50 +1,67 @@
 package in.co.nmsworks.training;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+public interface Jdbc{
 
-
-public class Jdbc {
-    public static void main(String[] args)
-    {
-        List<String> query=new ArrayList<>();
-       // query.add(new String[]{'kabi','writing homework'});
-        try {
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/Training");
-         //   PreparedStatement stmt = con.prepareStatement("insert into TodoList);
-
-//            String[] arr={"INSERT INTO TodoList values(1,'kabi','writingwork','2022-10-07')\");",};
-            for(int i=0;i<5;i++)
-            {
-
-
-
-
-//                PreparedStatement stmt = con.prepareStatement("INSERT INTO TodoList values(1,'kabi','writingwork','2022-10-07')");
-//                stmt.executeUpdate();
-//
-//                stmt = con.prepareStatement("INSERT INTO TodoList values(2,'appu','writingwork1','2022-10-07')");
-//                stmt.executeUpdate();
-//
-//                stmt = con.prepareStatement("INSERT INTO TodoList values(3,'dd','writingwork2','2022-10-07')");
-//                stmt.executeUpdate();
-//
-//                stmt = con.prepareStatement("INSERT INTO TodoList values(4,'aju','writingwork3','2022-10-07')");
-//                stmt.executeUpdate();
-//
-//                stmt = con.prepareStatement("INSERT INTO TodoList values(5,'ajuu','writingwork4','2022-10-07')");
-//                stmt.executeUpdate();
-            }
-
-        } catch (Exception e)
+        public static void main(String[] args)
         {
-            System.err.println("not created");
-            e.printStackTrace();
+            Connection con=null;
+            PreparedStatement stmt=null;
+            try {
+
+                con = DriverManager.getConnection("jdbc:mysql://localhost/Training");
+
+                List<String[]> queries=new ArrayList<>();
+                queries.add(new String[]{"Divya", "Do HW"});
+                queries.add(new String[]{"Priya", "Writing"});
+                queries.add(new String[]{"Keerthana","Do HW"});
+                queries.add(new String[]{"Aju","Writing"});
+                queries.add(new String[]{"Swathe","hw"});
+                queries.add(new String[]{"kavitha","work"});
+                queries.add(new String[]{"Muthu","Writing"});
+                queries.add(new String[]{"Santhosh","hw"});
+                queries.add(new String[]{"kumar","hw"});
+                queries.add(new String[]{"Kabila","hw"});
+
+                stmt=con.prepareStatement("INSERT INTO TodoList VALUES(?,?,?,?)");
+                for(int i=0;i< queries.size();i++)
+                {
+                    stmt.setInt(1,i+1);
+                    stmt.setString(2,queries.get(i)[0]);
+                    stmt.setString(3,queries.get(i)[1]);
+                    stmt.setDate(4,new Date(System.currentTimeMillis()));
+                    stmt.executeUpdate();
+                }
+
+
+            }
+            catch(Exception e)
+            {
+                System.err.println("ERROR occured");
+                e.printStackTrace();
+            }
+            finally {
+                if (stmt != null)
+                {
+                    try {
+                        stmt.close();
+                    } catch (SQLException e)
+                    {
+                        System.out.println("Catch");
+                    }
+                }
+
+                if (con != null)
+                {
+                    try {
+                        con.close();
+                    }
+                    catch (SQLException e)
+                    {
+                        System.out.println("Catch");
+                    }
+                }
+            }
         }
-
-
     }
-}
