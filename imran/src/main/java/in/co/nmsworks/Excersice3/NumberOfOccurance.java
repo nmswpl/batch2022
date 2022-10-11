7dce3447cfbda1b53641fabdef4286e9430905c3
Mohamed Imran <mohamedsubaideenimran@nmsworks.co.in>
2022-10-11 16:13:23 +0530

    Reading file and find NumberOfOcurance

--- imran/src/main/java/in/co/nmsworks/Excersice3/NumberOfOccurance.java
+++ imran/src/main/java/in/co/nmsworks/Excersice3/NumberOfOccurance.java
@@ -8,11 +8,7 @@ public class NumberOfOccurance
 {
     public static void numberOfOccurance(List<Integer> orglist) {
         Map<Integer, Integer> mymap = new HashMap<>();
-        List<Integer> one = new ArrayList<>();
-        List<Integer> two = new ArrayList<>();
-        List<Integer> three = new ArrayList<>();
-        List<Integer> four = new ArrayList<>();
-        List<Integer> five = new ArrayList<>();
+
         for (int i : orglist) {
             int count = 0;
             for (int j : orglist) {
@@ -28,50 +24,24 @@ public class NumberOfOccurance
         System.out.println("Count : "+mymap);
 
         Map<Integer,List<Integer>> ansmap = new HashMap<>();
-        for (Map.Entry<Integer, Integer> entry : mymap.entrySet()) {
-            if(entry.getValue() == 1)
-            {
-                one.add(entry.getKey());
-                ansmap.put(entry.getValue(), one );
-            }
-            else if (entry.getValue() == 2)
-            {
-                two.add(entry.getKey());
-                ansmap.put(entry.getValue(), two);
-            }
-            else if (entry.getValue() == 3)
-            {
-                three.add(entry.getKey());
-                ansmap.put(entry.getValue(), three);
-            }
-            else if (entry.getValue() == 4)
-            {
-                four.add(entry.getKey());
-                ansmap.put(entry.getValue(),four);
+        Set<Integer> myset = new HashSet<>(mymap.values());
 
-            }
-            else
+        for (int i : myset)
+        {
+            List<Integer> listforOccurance = new ArrayList<>();
+            for (Map.Entry<Integer, Integer> entry : mymap.entrySet())
             {
-                five.add(entry.getKey());
-                ansmap.put(entry.getValue(),five);
-
+                if(entry.getValue() == i)
+                {
+                    listforOccurance.add(entry.getKey());
+                }
             }
-
+            ansmap.put(i,listforOccurance);
+            System.out.println("\n");
+            System.out.println(i+" time  = "+ansmap.get(i));
         }
         System.out.println("\n");
         System.out.println("Counting in map : " +ansmap);
-        System.out.println("\n");
-        System.out.println("One time  = "+ansmap.get(1));
-        System.out.println("\n");
-        System.out.println("Two time  = "+ansmap.get(2));
-        System.out.println("\n");
-        System.out.println("Three time  = "+ansmap.get(3));
-        System.out.println("\n");
-        System.out.println("Four time  = "+ansmap.get(4));
-        System.out.println("\n");
-        System.out.println("Others   = "+ansmap.get(5));
-        System.out.println("\n");
-
 
     }
 }
