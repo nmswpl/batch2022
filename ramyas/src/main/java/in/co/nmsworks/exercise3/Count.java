5e730337aaf41276e13b84d327089b94b4abda2f
sramya <sramya@nmsworks.co.in>
2022-10-11 17:40:48 +0530

    Exercise 3 count the individual digit

--- ramyas/src/main/java/in/co/nmsworks/exercise3/Count.java
+++ ramyas/src/main/java/in/co/nmsworks/exercise3/Count.java
@@ -1,5 +1,6 @@
 package in.co.nmsworks.exercise3;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,15 +19,30 @@ public class Count {
                countdigit.put(i,countdigit.get(i)+1);
 
             }
-//            for (int j:list1)
-//            {
-//                if(i==j)
-//                    count++;
-//            }
+
             else
               countdigit.put(i,1);
         }
-        System.out.println(countdigit);
+       // System.out.println(countdigit);
+
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (Map.Entry<Integer, Integer> i : countdigit.entrySet())
+        {
+            if (map.containsKey(i.getValue()))
+            {
+                map.get(i.getValue()).add(i.getKey());
+            }
+            else
+            {
+                List<Integer>list = new ArrayList<>();
+                map.put(i.getValue(),list);
+                map.get(i.getValue()).add(i.getKey());
+            }
+        }
+
+        System.out.println("\n" +map );
     }
 }
 
