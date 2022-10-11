fbbbee4d06dc08505600bea2e0d3bbb54bab18f9
renuga <renugag@nmsworks.co.in>
2022-10-11 07:47:02 +0530

    changes done to fix program

---
+++ renuga/src/main/java/in/co/nmsworks/excercise2/EvenCount15.java
@@ -0,0 +1,13 @@
+package in.co.nmsworks.excercise2;
+
+public class EvenCount15 {
+    public static void main(String[] args) {
+        int a[]={1,2,5,6,3,2};
+        System.out.println("Even Numbers:");
+        for(int i=0;i<a.length;i++){
+            if(a[i]%2==0){
+                System.out.println(a[i]);
+            }
+        }
+    }
+}
