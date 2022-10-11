f5242938667f096db7e7f191e1046c17c67c6edb
VigneshV <vigneshv@nmsworks.co.in>
2022-10-11 11:05:41 +0530

    Find Duplicates in a File

--- vigneshV/src/main/java/in/co/nmsworks/exercisethree/ReadFile.java
+++ vigneshV/src/main/java/in/co/nmsworks/exercisethree/ReadFile.java
@@ -1,27 +1,25 @@
 package in.co.nmsworks.exercisethree;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReadFile {
-        public List<Integer> fileParser(String filepath) throws IOException
+        public List<Integer> fileParser(String filepath)
         {
-        FileReader read = new FileReader(filepath);
-        BufferedReader br = new BufferedReader(read);
-        String i;
         List<Integer> numbersList = new ArrayList<>();
-        ArrayList<Integer> duplicateNumbers = new ArrayList<>();
-        while ((i = br.readLine()) != null)
-        {
-            numbersList.add(Integer.valueOf(i));
+        try ( FileReader read = new FileReader(filepath);
+              BufferedReader br = new BufferedReader(read)){
+                String i;
+                while ((i = br.readLine()) != null)
+                {
+                        numbersList.add(Integer.valueOf(i));
+                }
         }
-
-
-        br.close();
+        catch (Exception e) {
+                e.printStackTrace();
+        }
         return numbersList;
     }
 }
