183a77aeaf2a1e13d55d16eb95893382e0ea11c0
Ajanthamahalakshmi <Ajanthamahalakshmi@nmsworks.co.in>
2022-10-11 17:20:23 +0530

    File Excercise

--- Ajanthamahalakshmi/src/main/java/in/co/nmsworks/excercise3/FileIntegerValueOccurrenceMap.java
+++ Ajanthamahalakshmi/src/main/java/in/co/nmsworks/excercise3/FileIntegerValueOccurrenceMap.java
@@ -1,38 +1,70 @@
 package in.co.nmsworks.excercise3;
 
+import noOccurrenceMap.Entry;
+
 import java.util.*;
 
-public class FileIntegerValueOccurrenceMap
-{
+public class FileIntegerValueOccurrenceMap {
 
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         String filePath = "/home/nmsadmin/Downloads/Numbers.txt";
-        FileRead fr = new FileRead();
-        List<Integer> originalIntegerList = new ArrayList<>(fr.readFileAsInt(filePath));
-        System.out.println(" Original List : "+originalIntegerList);
+        List<Integer> originalIntegerList = new ArrayList<>(FileRead.readFileAsInt(filePath));
+        System.out.println(" Original List : " + originalIntegerList);
 
-        Map<Integer,Integer> noOccurrenceMap = new HashMap<>();
+        Map<Integer, Integer> noOccurrenceMap = new HashMap<>();
 
         // 1, 2, 1, 3,4, 5, 3
 
-        for (int i : originalIntegerList )
-        {
+        for (int i : originalIntegerList) {
             int count = 0;
-            for(int j : originalIntegerList)
-            {
-                if(i == j)
-                {
+            for (int j : originalIntegerList) {
+                if (i == j) {
                     count++;
                 }
 
             }
-            noOccurrenceMap.put(i,count);
+            noOccurrenceMap.put(i, count);
 
         }
 
-        System.out.println("Map : "+noOccurrenceMap);
+        System.out.println("Map : " + noOccurrenceMap);
+
+        System.out.println("------------------------------------------------------------------------------");
+
+//
 
 
-    }
-}
+//        for(int value : noOccurrenceMap.values())
+        {
+
+
+            Map<Integer, List<Integer>> countKeyMap = new HashMap<>();
+
+            for (Map.Entry<Integer, Integer> entry : noOccurrenceMap.entrySet())
+            {
+                if (countKeyMap.containsKey(entry.getValue()))
+                {
+
+                    List<Integer> keyList = countKeyMap.get(entry.getValue());
+                    keyList.add(entry.getKey());
+
+                } else {
+                    List<Integer> keyList = new ArrayList<>();
+                    keyList.add(entry.getKey());
+                    countKeyMap.put(entry.getValue(), keyList);
+                }
+
+            }
+            System.out.println("Counted Key Value Map : "+countKeyMap);
+        }
+
+        }
+
+
+        }
+
+//        System.out.println(keyList);
+
+
+
+
