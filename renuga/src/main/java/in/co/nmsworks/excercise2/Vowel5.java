6d80bab89248aaff56ad765e626928aac2c3ee5d
renuga <renugag@nmsworks.co.in>
2022-10-11 07:41:37 +0530

    changes done to fix program

---
+++ renuga/src/main/java/in/co/nmsworks/excercise2/Vowel5.java
@@ -0,0 +1,16 @@
+package in.co.nmsworks.excercise2;
+
+/*5. Write a program to check if a given Character is a Vowel or Consonant.*/
+
+public class Vowel5 {
+    public static void main(String[] args) {
+        char ch ='r';
+
+        if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'){
+            System.out.println(ch + " is vowel ");
+        }
+        else {
+            System.out.println(( ch + " is Constant"));
+        }
+    }
+}
