837f65f77df1462d06ce2d5d8a4464a29b27e87e
sujitha <sujitha@nmsworks.co.in>
2022-10-11 13:08:18 +0530

    commit for file read

--- sujitha/src/main/java/in/co/nmsworks/exercise3/FileReading.java
+++ sujitha/src/main/java/in/co/nmsworks/exercise3/FileReading.java
@@ -1,29 +1,46 @@
 package in.co.nmsworks.exercise3;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class FileReading
-{
-    public static List<Integer> readFile(String path) throws Exception
-    {
-        BufferedReader br=new BufferedReader(new FileReader(path));
+public class FileReading {
+    public static List<Integer> readFile(String path) throws IOException {
         List<Integer> listOfNumbers = new ArrayList<>();
-        String numbers;
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-        while ((numbers=br.readLine())!= null)
-        {
-            int numbers1=Integer.parseInt(numbers);
-            listOfNumbers.add(numbers1);
+            String numbers;
+            while ((numbers = br.readLine()) != null) {
+                int numbers1 = Integer.parseInt(numbers);
+                listOfNumbers.add(numbers1);
+            }
         }
         return listOfNumbers;
     }
 
-    public static void main(String[] args) throws Exception
-    {
-      FileReading fr=new FileReading();
-      List<Integer> result = fr.readFile("/home/nmsadmin/Downloads/Numbers.txt");
-      System.out.println(result);
+    public void countOccurrenceOfElement(List<Integer> result) {
+        Map<Integer, Integer> numberWithCount = new HashMap<>();
+        for (int i = 0; i < result.size(); i++) {
+            if (numberWithCount.containsKey(result.get(i))) {
+                int values = numberWithCount.get(result.get(i));
+
+                numberWithCount.put(result.get(i), values + 1);
+            } else {
+                numberWithCount.put(result.get(i), 1);
+            }
+        }
+        System.out.println(numberWithCount);
+        System.out.println(numberWithCount.size());
+    }
+
+    public static void main(String[] args) throws IOException {
+        FileReading fr = new FileReading();
+        List<Integer> result = fr.readFile("/home/nmsadmin/Downloads/Numbers.txt");
+        //System.out.println(result);
+        fr.countOccurrenceOfElement(result);
     }
 }
