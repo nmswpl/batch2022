39f4010700088cda0965bb5d8adc3a68139d42cc
DivyaDharshini <divyadharshini@nmsworks.co.in>
2022-10-11 16:40:43 +0530

    Excercise3 NumberOfOccurance Update

--- DivyaDharshini/src/main/java/in/co/nmsworks/excercise3/NumberOfOccurance.java
+++ DivyaDharshini/src/main/java/in/co/nmsworks/excercise3/NumberOfOccurance.java
@@ -1,26 +1,44 @@
 package in.co.nmsworks.excercise3;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class NumberOfOccurance {
-    public int numberOfOccurance(List<Integer> original)
-    {
-        Map<Integer,Integer> integerMap=new HashMap<>();
+    public int numberOfOccurance(List<Integer> original) {
+        Map<Integer, Integer> integerMap = new HashMap<>();
+
         for (int i : original) {
-            if(integerMap.containsKey(i))
-            {
-                int count= integerMap.get(i);
-                integerMap.put(i,count+1);
-            }
+            if (integerMap.containsKey(i)) {
+                int count;
+                count = integerMap.get(i);
+                integerMap.put(i, count + 1);
+                }
             else
             {
-                integerMap.put(i,1);
+                    integerMap.put(i, 1);
+                }
+             }
+
+            Map<Integer,List<Integer>> listMap=new HashMap<>();
+            for(Map.Entry<Integer,Integer> entry: integerMap.entrySet())
+            {
+                int value=entry.getValue();
+                int keyValue=entry.getKey();
+                if(listMap.containsKey(value))
+                {
+                    listMap.get(value).add(keyValue);
+                }
+                else
+                {
+                    listMap.put(value,new ArrayList<>());
+                    listMap.get(value).add(keyValue);
+                }
             }
-        }
-        System.out.println("\nCount : "+integerMap);
-        return integerMap.size();
+            System.out.println("\nCount : " + integerMap);
+            System.out.println(listMap);
+            return integerMap.size();
     }
     public static void main(String[] args) throws Exception {
         CreateFile createFile =new CreateFile();
