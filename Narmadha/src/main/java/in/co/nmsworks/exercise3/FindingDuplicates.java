477e56b7e14ad51be6a89062150bddc783e3b534
nmsadmin <narmadhas@nmsworks.co.in>
2022-10-11 13:03:47 +0530

    occurance of particular number

--- Narmadha/src/main/java/in/co/nmsworks/exercise3/FindingDuplicates.java
+++ Narmadha/src/main/java/in/co/nmsworks/exercise3/FindingDuplicates.java
@@ -1,9 +1,7 @@
 package in.co.nmsworks.exercise3;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static in.co.nmsworks.exercise3.FileParse.readTheFile;
 
@@ -12,6 +10,7 @@ public class FindingDuplicates {
 
         try {
             List<Integer>intList=readTheFile("/home/nmsadmin/Downloads/Numbers.txt");
+            System.out.println(intList);
             Set<Integer>duplicates=new HashSet<>();
             int count = 0;
             for (int num:intList)
@@ -27,7 +26,23 @@ public class FindingDuplicates {
                     duplicates.add(num);
                 }
             }
-            System.out.println(count);
+            System.out.println("duplicate values are"+count);
+
+
+            Map<Integer,Integer>OccuranceOfNumber=new HashMap<>();
+           for(int i:intList){
+               if(OccuranceOfNumber.containsKey(i)){
+                   OccuranceOfNumber.put(i,OccuranceOfNumber.get(i)+1);
+
+               }
+               else{
+                   OccuranceOfNumber.put(i,1);
+               }
+
+           }
+            System.out.println(OccuranceOfNumber);
+
+
 
         } catch (Exception e) {
             throw new RuntimeException(e);
