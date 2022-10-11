d0323f23bd6cf1cc4bc455f92c331d25d6802a60
renuga <renugag@nmsworks.co.in>
2022-10-11 07:47:20 +0530

    changes done to fix program

---
+++ renuga/src/main/java/in/co/nmsworks/excercise2/Factorial16.java
@@ -0,0 +1,16 @@
+package in.co.nmsworks.excercise2;
+
+public class Factorial16 {
+    public static void main(String[] args) {
+        int num = 6;
+        long factorial = multiplyNumbers(num);
+        System.out.println("Factorial of " + num + " = " + factorial);
+    }
+    public static long multiplyNumbers(int num)
+    {
+        if (num >= 1)
+            return num * multiplyNumbers(num - 1);
+        else
+            return 1;
+    }
+}
