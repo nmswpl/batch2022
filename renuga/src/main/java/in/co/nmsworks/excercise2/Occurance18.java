deaa69af0022edf96a4c31c3dc5929386e4d0f5d
renuga <renugag@nmsworks.co.in>
2022-10-11 07:50:12 +0530

    changes done to fix program

---
+++ renuga/src/main/java/in/co/nmsworks/excercise2/Occurance18.java
@@ -0,0 +1,31 @@
+package in.co.nmsworks.excercise2;
+
+import java.util.Scanner;
+
+public class Occurance18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        int n, x, count = 0, i = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no of elements you want in array:");
+        n = s.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter all the elements:");
+        for(i = 0; i < n; i++)
+        {
+            a[i] = s.nextInt();
+        }
+        System.out.print("Enter  number of  Count occurrences:");
+        x = s.nextInt();
+        for(i = 0; i < n; i++)
+        {
+            if(a[i] == x)
+            {
+                count++;
+            }
+        }
+        System.out.println("Number of Occurrence of the Element:"+count);
+    }
+}
