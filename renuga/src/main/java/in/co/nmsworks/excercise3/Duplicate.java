8e8b950ef71c8d3c31a9b0af96669dbdd22533f1
renuga <renugag@nmsworks.co.in>
2022-10-11 07:36:11 +0530

    changes done to fix program
    commit the file into program

---
+++ renuga/src/main/java/in/co/nmsworks/excercise3/Duplicate.java
@@ -0,0 +1,37 @@
+package in.co.nmsworks.excercise3;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Duplicate {
+    public void Duplicates(List<Integer> arrayList)
+    {
+        Set<Integer> uniqueSet = new HashSet<>();
+
+        int duplicateCount =0 ;
+
+        for (int i=0 ; i < arrayList.size() ; i++)
+        {
+            int item = arrayList.get(i);
+            if(uniqueSet.contains(item))
+            {
+                duplicateCount++;
+                System.out.println(item + " is Duplicate");
+            }
+            else
+            {
+                uniqueSet.add(item);
+            }
+        }
+        System.out.println("duplicate : " +  duplicateCount);
+    }
+
+    public static void main(String[] args) {
+        Duplicate instance = new Duplicate();
+        List<Integer> abc = FileParsing.readFileAndGetIntList("/home/nmsadmin/Downloads/Numbers.txt");
+        instance.Duplicates(abc);
+    }
+}
+
+
