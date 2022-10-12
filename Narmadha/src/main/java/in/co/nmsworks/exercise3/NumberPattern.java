b9be4b05fd51792e317582739883782f42e9ca7c
nmsadmin <narmadhas@nmsworks.co.in>
2022-10-12 23:22:29 +0530

    Pattern program

---
+++ Narmadha/src/main/java/in/co/nmsworks/exercise3/NumberPattern.java
@@ -0,0 +1,25 @@
+package in.co.nmsworks.exercise3;
+
+import java.util.Scanner;
+
+public class NumberPattern{
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter no:");
+        int no= input.nextInt();
+        int a=97;
+        for (int i = 0; i <no; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print((char)(a+i)+" ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+    }
+}
