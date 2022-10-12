fd2af06d617e3710b87b2fd9ac5238a7b727d200
VigneshV <vigneshv@nmsworks.co.in>
2022-10-12 12:47:38 +0530

    Files Creation

---
+++ vigneshV/src/main/java/in/co/nmsworks/Training/CreateFiles.java
@@ -0,0 +1,57 @@
+package in.co.nmsworks.Training;
+
+import java.io.*;
+import java.sql.ResultSet;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateFiles {
+    public static void genderFile(Statement st) throws SQLException, IOException
+    {
+        ResultSet s = st.executeQuery("select * from user_details");
+
+        try
+                (
+                        BufferedWriter maleFileWriter = new BufferedWriter(new FileWriter("/home/nmsadmin/git/batch2022/vigneshV/src/main/java/in/co/nmsworks/Training/Male.txt"));
+                        BufferedWriter femaleFileWriter = new BufferedWriter(new FileWriter("/home/nmsadmin/git/batch2022/vigneshV/src/main/java/in/co/nmsworks/Training/Female.txt"))
+                )
+        {
+            while (s.next()) {
+                if (s.getString(5).equals("Male"))
+                {
+                    String userName = s.getString(2);
+                    String firstName = s.getString(5);
+                    String lastName = s.getString(4);
+
+
+                    maleFileWriter.write(userName + " " + firstName + " " + " " + lastName + "\n");
+                }
+                else
+                {
+                    String userName = s.getString(2);
+                    String firstName = s.getString(5);
+                    String lastName = s.getString(4);
+                    femaleFileWriter.write(userName + " " + firstName + " " + " " + lastName + "\n");
+                }
+            }
+        }
+
+    }
+
+    public static void loginTimeFile(Statement st) throws SQLException, IOException
+    {
+        ResultSet s = st.executeQuery("SELECT first_name,last_name FROM user_details   WHERE   EXISTS (SELECT 1 FROM user_login WHERE user_details.user_id = user_login.id)");
+
+        try(BufferedWriter loginFileWriter = new BufferedWriter(new FileWriter("/home/nmsadmin/git/batch2022/vigneshV/src/main/java/in/co/nmsworks/Training/loginuser.txt")))
+        {
+            while (s.next())
+                loginFileWriter.write(s.getString(1)+' '+s.getString(2)+'\n');
+        }
+
+
+    }
+}
+
