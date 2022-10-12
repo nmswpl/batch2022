7a7270df11f23717df2619c04797cc67d63a5956
renuga <renugag@nmsworks.co.in>
2022-10-12 17:52:20 +0530

    changes done to fix program

--- renuga/src/main/java/in/co/nmsworks/training/Pattern.java
+++ renuga/src/main/java/in/co/nmsworks/training/Pattern.java
@@ -4,8 +4,6 @@ public class Pattern {
     public static void main(String[] args) {
         int size = 5;
         char ch='a';
-
-
         for (int i = 0; i < size; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print((char)(ch+i));
