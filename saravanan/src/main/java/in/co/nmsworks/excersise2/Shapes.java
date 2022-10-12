e59dc7443165659a3b495a706bc2528f286cdc4e
saravanan <saravanans@nmsworks.co.in>
2022-10-12 12:41:49 +0530

    triangle shapes

--- saravanan/src/main/java/in/co/nmsworks/excersise2/Shapes.java
+++ saravanan/src/main/java/in/co/nmsworks/excersise2/Shapes.java
@@ -1,2 +1,19 @@
-package in.co.nmsworks.excersise2;public class PramidShape {
+package in.co.nmsworks.excersise2;
+
+
+
+public class Shapes {
+    public static void main(String[] args)
+    {
+        int number =5;
+        char[] a ={'a','b','c','d','e','f','g','h','i','k','o','n','m'};
+        for (char i =0; i <number; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print(a[i]);
+            }
+            System.out.println();
+        }
+    }
 }
