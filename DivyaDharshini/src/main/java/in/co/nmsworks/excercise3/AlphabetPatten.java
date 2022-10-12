44163c0f7384e03b16c969e5ba040ade4157073a
DivyaDharshini <divyadharshini@nmsworks.co.in>
2022-10-12 14:34:58 +0530

    Login User Details , AlphabetPatten

--- DivyaDharshini/src/main/java/in/co/nmsworks/excercise3/AlphabetPatten.java
+++ DivyaDharshini/src/main/java/in/co/nmsworks/excercise3/AlphabetPatten.java
@@ -2,21 +2,18 @@ package in.co.nmsworks.excercise3;
 import java.util.Scanner;
 
 public class AlphabetPatten {
-        public static void main(String[] args)
-        {
+        public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter");
             int n = sc.nextInt();
-            pattern(n);
-        }
-        private static void pattern(int n)
-        {
-            int i,j;
-            for(i=0;i<=n;i++)
-            {
-                for( j = 1; j<=i; j++) {
+            int i, j;
+            for (i = 0; i <= n; i++) {
+                for (j = 1; j <= i; j++) {
                     System.out.print((char) (96 + i));
                 }
+                System.out.println();
             }
+
         }
+
 }
