b7d1e0bbb72886896e18cbf15589d57f84740dfa
Shanmugapriya <shanmugapriya@nmsworks.co.in>
2022-10-12 14:07:43 +0530

    Commit changes
    Print the character in a sequence

---
+++ Shanmugapriya/src/main/java/in/co/nmsworks/excercise3/PrintingCharacters.java
@@ -0,0 +1,23 @@
+package in.co.nmsworks.excercise3;
+
+import java.util.Scanner;
+
+public class PrintingCharacters
+{
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter the number of Character :");
+        int num = S.nextInt();
+
+        for(int i = 0 ; i < num; i++)
+        {
+            for(int j = 0 ; j <= i; j++)
+            {
+                char ch = (char)(65+i);
+                ch = Character.toLowerCase(ch);
+                System.out.print(" "+ch);
+            }
+            System.out.println();
+        }
+    }
+}
