ab519aaeb071bd729e583a4fef657ed18fae98da
admin <sakthivelm@nmsworks.co.in>
2022-10-18 11:40:47 +0530

    Titanic

---
+++ sakthivel/src/main/java/in/co/nmsworks/exercise4/Passenger.java
@@ -0,0 +1,63 @@
+package in.co.nmsworks.exercise4;
+
+public class Passenger
+{
+    public Integer passengerId;
+    public Integer survived;
+    public Integer pClass;
+    public String  name;
+    public String sex;
+    public Float
+            age;
+
+    public void setPassengerId(Integer passengerId) {
+        this.passengerId = passengerId;
+    }
+
+    public void setSurvived(Integer survived) {
+        this.survived = survived;
+    }
+
+    public void setpClass(Integer pClass) {
+        this.pClass = pClass;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(Float age) {
+        this.age = age;
+    }
+
+    public Integer getPassengerId() {
+        return passengerId;
+    }
+
+    public Integer getSurvived() {
+        return survived;
+    }
+    public Integer getpClass() {
+        return pClass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+    public Float getAge() {
+        return age;
+    }
+
+
+
+
+
+}
